ae9a7fadc521418e1eb523382adde4c719c41d45
rajkkkkkkkk <96653626+rajkkkkkkkk@users.noreply.github.com>
2022-01-14 12:59:08 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,171 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+     <!-- saved from url=(0062)http://innovationagro.com/vijayarora/agnext_signature_new.html -->
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<style>
+<!-- *{font-family:sans-serif;}
+span{font-size:13px;}
+p{font-size:13px; color:#949597;padding: 0;margin:4px 0 0;}
+.sa_signature_area{max-width:800px; float:left; width:100%;}
+.pull-left{float:left;}
+.col-12{width:600px;}
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
 
-      <Footer />
+.tb1_left{float:right; width:300px;}
+.tb1_right{overflow:hidden}
+.tb2_left{float:left; width:300px;  padding-top:10px;}
+.tb2_right{overflow:hidden; background:#f2f2f2; padding:10px 10px;}
+.mob_area{color:#949597!important;}
+.mob_area b{color:#009400!important}
+.mob_area img{ width:30px;float:left;}
+table.tb2_left.bgimg {position: relative;background:url("http://agnext.in/images/shapebgimg.png") 0 0 no-repeat;}
+
+
+@media (max-width: 768px){
+	
+.tb1_left{float:right; width:100%;  margin:0 0 10px 0; text-align:left;}
+.tb1_right{float:left; width:100%;  margin:0 0 10px 0;}
+.tb2_left{float:left; width:100%; margin:0 0 10px 0;}
+.tb2_right{float:left; width:100%; }
+.text_area{margin-top:10px;}
+
+} -->
+</style>
+
+</head>
+<body>
+<table style="width:646px" cellSpacing=0 cellPadding=0 border=0>
+	<tr>
+	
+				<td style="vertical-align: text-top;">
+					<table class="tb1_left" style="float:left; width:120px;padding-right:0px;box-sizing:border-box;text-align: center;">
+						<tr>
+							<td>
+								<img style="width: 100%;float: none;max-width:88px;;box-sizing: border-box;" src="http://agnext.in/images/boby.png">
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<table class="mob_area" style="display:table;margin:0 auto;width:60px;padding-top:0px;text-align: center;padding-right:0px;box-sizing: border-box;padding:0px;">	
+									<tr>
+									<td class="mob_area" style="color:#949597!important; padding:0px 0;display:inline-table; width:22px; line-height:18px; font-size:16px;">
+										<a href="https://www.linkedin.com/in/gautam-bhuyan-104a0019b/" target="_blank" style="display:inline-table;width:20px;"><img style="width:20px; display:inline-table;" src="http://agnext.in/images/socialiconblackb.png"></a>
+									</td>
+									<!-- <td class="mob_area" style="color:#949597!important; padding:0px 0;display:inline-table; width:22px; line-height:18px; font-size:16px;"> -->
+										<!-- <a href="#" target="_blank" style="display:inline-table;width:20px;"><img style="width:20px; display:inline-table;" src="http://agnext.in/images/socialiconblackc.png"></a> -->
+									<!-- </td> -->
+									</tr>
+								</table>
+							</td>
+						</tr>
+					</table>
+				</td>
+			<td>
+				<table class="tb1_right" style="width:550px; float:left;">
+					<tr>
+						<td>
+							<table style="width:500px; float:left;" cellSpacing="0" cellPadding="0" border="0">
+								<tr>
+									<td>
+										<h2 style="color:#009400; margin:0px 0 0px 0; float:left; width:100%; font-size:20px;line-height:22px;">Gautam Bhuyan</h2>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<span style="font-size:14px; margin:0px 0 0px 0; float:left; width:100%; color:#53585f;line-height: 22px;font-weight: bold;padding-left: 5px;">Business Head</span>
+									</td>
+								</tr>
+							</table>
+						</td>
+				</tr>
+					
+					<tr>	
+						<td>
+						
+						
+						
+						<table class="mob_area" style="float:left; width:550px;padding-top:0px;" border="0">
+							<tr>
+								<td class="mob_area" style="color:#009400!important; padding:6px 0; min-height:20px; float:left; width:136px; line-height:18px; font-size:14px;">
+									<table>
+										<tr>
+											<td><img style="margin:2px 2px 0 0; width:16px; float:left;" src="http://agnext.in/images/newmob-icon.png"></td>
+											<td><a href="#" style="float: left;color:#009400;padding-left:0px;text-decoration: none;font-size:14px;">+91 9831093669</a></td>
+										</tr>
+									</table>
+								</td>
+								<td class="mob_area" style="color:#009400!important; padding:6px 0;float:left; width:202px; line-height:18px; font-size:16px;">
+									<table>
+										<tr><td><img style="margin:2px 2px 0 0; width:16px; float:left;" src="http://agnext.in/images/newemail-icon.png"></td>
+										<td><a href="#" style="color:#009400 !important;font-size:14px;text-decoration:none;">gautam@agnext.com</a></td>
+										</tr>
+									</table>
+								</td>
+
+								<td class="mob_area" style="color:#009400!important;padding:6px 0 0; width:160px;  line-height:18px; font-size:14px;float:right;">
+									<a href="http://agnext.com/" target="_blank" style="float: left;"><img style="width:160px;float: right;max-width: 100%;" src="http://agnext.in/images/companylogo.png"></a>
+								</td>
+							</tr>
+						</table>
+						</td>
+					</tr>	
+					<tr>	
+					<td>
+						<table class="tb2_left bgimg" style="float:left;width:550px;padding-top:0px;padding:0 0;height:30px;background:url(http://agnext.in/images/shapebgimg.png) no-repeat;background-size:contain;background:#079547;"  background="http://agnext.in/images/shapebgimg.png" vertical-align="middle">
+								<tr>
+									<td class="mob_area" style="color:#fff!important;min-height:20px;  float:left; width:150px;  line-height:16px; font-size:14px;padding:2px 0 0 10px;">
+										<table>
+											<tr>
+											<td><img style="width:16px;float:left;"src="http://agnext.in/images/websitelink.png"></td>
+											<td><a href="http://agnext.com/" target="_blank" style="float: left;color:#fff;padding-left:3px;text-decoration:none;font-size:14px;"><img style="width:80px;float:left;height:14px;padding-top: 3px;"src="http://agnext.in/images/agnextcom.png"></a></td>
+											</tr>
+										</table>
+									</td>
+									<td class="mob_area" style="float:right;text-align: right;margin-right:0px;padding-right:26px;">
+										<table>
+											<tr>
+												<td class="mob_area" style="color:#949597!important; padding:0px 0;display:inline-block;width:24px; line-height:18px; font-size:16px;">
+													<a href="https://www.facebook.com/agnextworld" target="_blank" style="float: left;"><img style="width:20px; float:left;" src="http://agnext.in/images/socialiconwhited.png"></a>
+												</td>
+												<td class="mob_area" style="color:#949597!important; padding:0px 0;display:inline-block; width:24px; line-height:18px; font-size:16px;">
+													<a href="https://www.linkedin.com/company/agnextworld" target="_blank" style="float: left;"><img style="width:20px; float:left;" src="http://agnext.in/images/socialiconwhitea.png"></a>
+												</td>
+												<td class="mob_area" style="color:#949597!important; padding:0px 0;display:inline-block;width:24px; line-height:18px; font-size:16px;">
+													<a href="https://twitter.com/agnextworld" target="_blank" style="float: left;"><img style="width:20px; float:left;" src="http://agnext.in/images/socialiconwhitec.png"></a>
+												</td>
+												<td class="mob_area" style="color:#949597!important; padding:0px 0;display:inline-block; width:24px; line-height:18px; font-size:16px;">
+													<a href="https://www.instagram.com/agnextworld/" target="_blank" style="float: left;"><img style="width:20px; float:left;" src="http://agnext.in/images/socialiconwhiteb.png"></a>
+												</td>
+											</tr>
+										</table>
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+					
+					
+					<tr>
+						<td>
+							<table style="float:left; width:550px;" class="pull-left col-12 text_area" cellSpacing="0" cellPadding="0" border="0">
+								<tr>
+									<td>
+										<p style="font-size:7px; color:#dbdbdb; float:left; width:515px; margin:0 0 0px 0; padding:0px 0 0 0;line-height:8px;">This message contains confidential information and is intended only for the individual named. If you are not the named addressee you should not disseminate, distribute or copy this email. Please notify the sender immediately if you have received this email by mistake and delete this email from your system. If you are not the intended recipient you are notified that disclosing, copying or taking any action in reliance on the contents of this information is strictly prohibited</p>
+										<h3 style="font-size:10px;color:#cbcbcb;float:left;width:500px;margin: 0;">Save Resources, Think before you print.</h3>
+									</td>
+								</tr>	
+							</table>
+						</td>
+					</tr>
+				</table>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
     </div>
   )
 }
